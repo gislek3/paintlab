f065ee7365f95bdece9088e0511b25e9fecbb34d
contraposition <tumblrpod1@gmail.com>
2024-05-13 13:24:47 +0200

    Added base to step0

---
+++ model/ColorFactory.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.awt.Color;
+import java.util.Random;
+
+
+public class ColorFactory {
+    List<Color> colors = new ArrayList<>(Arrays.asList(Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN, Color.PINK));
+}
